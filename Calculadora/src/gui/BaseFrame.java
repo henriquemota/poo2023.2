81d2f88562d7f6c7ab222ebf178e3be375b27916
henriquemota <mota.henrique@gmail.com>
2023-10-02 09:46:35 -0300

    layout da calculadora finalizado

--- Calculadora/src/gui/BaseFrame.java
+++ Calculadora/src/gui/BaseFrame.java
@@ -5,14 +5,14 @@ import javax.swing.JFrame;
 public class BaseFrame extends JFrame {
 	
 	public BaseFrame() {
-		this.setSize(800,600);
+		//this.setSize(800,600);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
 	}
 	
 	public BaseFrame(String title) {
-		this.setSize(800,600);
+		//this.setSize(800,600);
 		this.setTitle(title);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
