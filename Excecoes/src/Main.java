4e149e7859523a0a372684331daee806b951679a
henriquemota <mota.henrique@gmail.com>
2023-10-16 08:36:00 -0300

    implementacao de tratamento de excecoes

--- Excecoes/src/Main.java
+++ Excecoes/src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args){
+		/*
 		try {
 			new Subprograma01().execute();
 		} catch (NumberFormatException e) {
@@ -10,6 +11,24 @@ public class Main {
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e.getMessage());
 			System.out.print(e.getMessage());
+		}
+		*/
+		
+		
+		try {
+			System.out.println("Executei a primeira linha");
+			//throw new MinhaExcecao();
+			int x [] = new int[2];
+			x[0] = 10;
+			x[1] = 20;
+			x[2] = 30;
+			//System.out.println("Executei a segunda linha");
+		} catch(RuntimeException e) {
+			System.out.println("tratamento de excecao do array");
+		} catch (Exception e) {
+			System.out.println(e.getClass());
+		} finally {
+			System.out.println("Executei a linha do finally");
 		}
 	}
 	
